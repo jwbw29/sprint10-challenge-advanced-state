32df0c38eb4b8e474b2e202cb663a82e0bb48563
Justin Byrd <justinbyrd7@gmail.com>
2023-10-25 09:21:22 -0500

    added mapping to Wheel from LearnBot

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -4,33 +4,36 @@ import { connect } from "react-redux";
 
 const Wheel = (props) => {
   const { wheel, moveClockwise, moveCounterClockwise } = props;
+  //// TODO I think we need to apply the state variable to these divs somehow; but that doesn't explain while it's not console logging onClick
 
   return (
-    //// TODO I think we need to apply the state variable to these divs somehow; but that doesn't explain while it's not console logging onClick
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>
+        <div
+          className={`cog active ${wheel === 0 ? "selected" : ""}`}
+          style={{ "--i": 0 }}
+        >
           B
         </div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>
-        {/** --i is a custom CSS property, no need to touch that nor the style object */}
+        {[1, 2, 3, 4, 5].map((cog) => (
+          <div
+            key={cog}
+            className={`cog ${wheel === cog ? "selected" : ""}`}
+            style={{ "--i": cog }}
+          ></div>
+        ))}
       </div>
       <div id="keypad">
         <button id="counterClockwiseBtn" onClick={() => moveCounterClockwise()}>
           Counter clockwise
         </button>
-        <button id="clockwiseBtn" onClick={() => moveClockwise(wheel)}>
+        <button id="clockwiseBtn" onClick={() => moveClockwise()}>
           Clockwise
         </button>
       </div>
     </div>
   );
 };
-
 const mapStateToProps = (state) => {
   return {
     wheel: state.wheel,
