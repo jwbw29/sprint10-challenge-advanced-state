87b1a947b688ac8b86c3005c1fd4a63e22fba259
Justin Byrd <justinbyrd7@gmail.com>
2023-10-25 10:41:03 -0500

    message might be done

--- frontend/components/Message.js
+++ frontend/components/Message.js
@@ -2,9 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 
 const Message = (props) => {
-  const {} = props;
-  // [ ] We'll remove "Nice job!" and replace with something passed from a prop
-  return <div id="message">Nice job!</div>;
+  const { infoMessage } = props;
+  // [x] We'll remove "Nice job!" and replace with something passed from a prop
+  return <div id="message">{infoMessage}</div>;
 };
 
 const mapStateToProps = (state) => {
